edce08350ff96a5d912922fc8b4cc7d5223da6e6
Isaac Pérez <56978632+Isaac13Di00@users.noreply.github.com>
2023-05-05 20:35:28 -0700

    Vista home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,54 @@
-@extends('layouts.app')
-
+@extends('layouts.base')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                  <img src="..." class="img-fluid rounded-start" alt="...">
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
+            <div class="col card mb-3 mt-3 me-3" style="max-width: 95%;">
+                <div class="card-body">
+                    <h4 class="card-title text-center">Nombre Generico Que Ira Aquí</h4>
+                    <h4 class="card-title">Membresia:</h4>
+                    <h5 class="card-text">Individual</h5>
+                    <h4 class="card-title">Miembro desde:</h4>
+                    <h5 class="card-text">2021-09-24</h5>
+                    <br>
+                    <h4 class="card-title">Vigencia:</h4>
+                    <h5 class="card-text">01-May-2022 hasta 01-Jun-2022</h5>
+                    <br>
+                    <h4 class="card-title">Sexo:</h4>
+                    <h5 class="card-text">Femenino</h5>
+                    <h4 class="card-title">Fecha de nacimiento:</h4>
+                    <h5 class="card-text">1996-01-06</h5>
+                    <h4 class="card-title">Tipo de sangre:</h4>
+                    <h5 class="card-text">O-</h5>
+                    <br>
+                    <h4 class="card-title">Telefono:</h4>
+                    <h5 class="card-text">6512156554</h5>
+                    <h4 class="card-title">Email:</h4>
+                    <h5 class="card-text">example@gmail.com</h5>
+                    <br>
+                    <h4 class="card-title">Dirrecion</h4>
+                    <h4 class="card-title">Calle:</h4>
+                    <h5 class="card-text">Calle 1</h5>
+                    <h4 class="card-title">Colonia:</h4>
+                    <h5 class="card-text">Col</h5>
+                    <h4 class="card-title">Codigo postal:</h4>
+                    <h5 class="card-text">00000</h5>
                 </div>
             </div>
-          </div>
+            <div class="col card mb-3 mt-3" style="max-width: 30%;">
+                <img src="/images/user-alt.png" class="mt-2 mb-2 card-img-top" alt="Este se supone que eres tu.">
+                <ul class="nav col-12 col-lg-auto my-2 justify-content-end my-md-0 text-small">
+                    <li>
+                        <a href="#" class="nav-link text-black">
+                            <i class="bi-person" style="font-size: 2rem; color: black;"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link text-black">
+                            <i class="bi-calendar-plus" style="font-size: 2rem; color: black;"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
     @vite(['resources/sass/home.scss', 'resources/js/app.js'])
-    
 </head>
 <body>
     <header>
@@ -14,7 +13,7 @@
           <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
               <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-                <img class="mb-4" src="{{ asset('images/LOGO_GYM.png') }}" alt="" width="50%" height="10%">
+                <img class="mb-4" src="{{ asset('images/LOGO_GYM.png') }}" alt="" width="50%">
               </a>
               <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
                 <li>
@@ -33,38 +32,26 @@
                   </a>
                 </li>
                 <li class="nav-item dropdown">
-                    <button class="btn dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="bi-list-ul" style="font-size: 2rem; color: white;"></i>
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-dark">
-                      <li><a class="dropdown-item" href="#">Action</a></li>
-                      <li><a class="dropdown-item" href="#">Another action</a></li>
-                    </ul>
-                  </li>          
+                  <button class="btn dropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                      <i class="bi-list-ul" style="font-size: 2rem; color: white;"></i>
+                  </button>
+                  <ul class="dropdown-menu dropdown-menu-dark">
+                    <li><a class="dropdown-item" href="#">Política de privacidad</a></li>
+                    <li><a class="dropdown-item" href="#">Cerrar Sesión</a></li>
+                  </ul>
+                </li>          
               </ul>
-            </div>
-          </div>
-        </div>
-        <div class="px-3 py-2 border-bottom mb-3">
-          <div class="container d-flex flex-wrap justify-content-center">
-            <form class="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto" role="search">
-              <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
-            </form>
-    
-            <div class="text-end">
-              <button type="button" class="btn btn-light text-dark me-2">Login</button>
-              <button type="button" class="btn btn-primary">Sign-up</button>
             </div>
           </div>
         </div>
       </header>
     <main>
         @yield('content')
-        {{-- @if ()
-            @yield('content-user')
-        @else
-            @yield('content-admin')
-        @endif --}}
     </main>
+    <footer class="p-auto m-auto" style="max-width: 90%;">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      </ul>
+      <p class="text-center text-body-secondary">Horario de atención de 8 am a 4 pm horario del centro de México. | soporte@krachtigym.mx | +52 998 187 8753</p>
+    </footer>
     </html>
 </body>
